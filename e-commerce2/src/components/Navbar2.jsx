61ac31ce8a56fa92a88739e3b5eee1c855643441
Khuslenu <khuslenu@MacBook-Pro-3.local>
2023-01-15 20:23:52 +0800

    i will learn

--- e-commerce2/src/components/Navbar2.jsx
+++ e-commerce2/src/components/Navbar2.jsx
@@ -3,26 +3,39 @@ import { Person, Heart, Cart } from 'react-bootstrap-icons';
 
 
 function Navbar2(props) {
-    const [ list, setlist] = useState(false)
+    const [list, setlist] = useState(false)
 
     return (
         <div className="d-flex mx-5 mt-5">
             <button className="btn text-white rounded-circle">
-                
+
                 <div className="m-2 " >
-                    <Heart className='wishlist-main' onClick={() => setlist(true)}/>
-                    {list ? <div className='wishlist-cat'>
-                            <h5>Wishlist</h5>
-                            {props.wishlist}
-                            <button className='close-button' onClick={() => { setlist(false) }}>Close</button>
-                        </div> : ""}
+                    <Heart className='wishlist-main' onClick={() => setlist(true)} />
+                    {list ? <div className='wishlist-cat text-black'>
+                        <h5>Wishlist</h5>
+                        <div>
+                            {props.products.map((element) => {
+                                <div>
+                                    <div>
+                                        <img src={element.image} className="rounded img-fluid" alt="" style={{ width: "80%" }} />
+                                    </div>
+                                    <div>
+                                        {element.name}
+                                    </div>
+                                    <div>
+                                        {element.price}
+                                    </div>
+                                </div>
+                            })}
+                        </div>
+                        <div>{props.wishlist}</div>
+                        <button className='close-button' onClick={() => { setlist(false) }}>Close</button>
+                    </div> : ""}
                     <div>
-                        {props.products.map((product) => {
-                        //   console.log(product.icon)
-                        })}
+
                         {props.wishlist}
                     </div>
-                  
+
                 </div>
             </button>
             <div className="m-1">
